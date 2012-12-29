ac82f1a347fd1c0c46b490c9767ccfbf21b2e0fd
Demeter Sztanko <we@sarok.org>
2012-12-29 20:13:16 +0000

    Added versioning and building of different types of the library the builder

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,23 +1,34 @@
 uglify="uglifyjs"
 #uglify="cat"
+version="0.1"
 
-rm -rf ../dist/crosslet-min.js
+d="../dist/$version/js"
+cxf="$d/crosslet.js"
+cx="$d/crosslet-raw.js"
+cxfm="$d/crosslet-min.js"
+cxm="$d/crosslet-raw-min.js"
+rm -rf $d
+mkdir -p $d
 for f in `find ../lib/ -type f -name "*.js" | sort`
 do
 echo $f
-cat $f | $uglify >> ../dist/crosslet-min.js
+cat $f  >> $cxf
 done
 
 for f in `find ../src/ -type f -name "*.js" | sort`
 do
 echo $f
-cat $f | $uglify >> ../dist/crosslet-min.js
+cat $f >> $cx
 done
 
-coffee -cbp ../src/coffee/init.coffee | $uglify >> ../dist/crosslet-min.js
+coffee -cbp ../src/coffee/init.coffee  >> $cx
 for f in `find ../src/ -type f -name "*.coffee" | sort | grep -v init.coffee`
 do
 echo $f
-echo "coffee -cbp $f | $uglify"
-coffee -cbp $f | $uglify >> ../dist/crosslet-min.js
+echo "coffee -cbp $f "
+coffee -cbp $f >> $cx
 done
+
+cat $cx >> $cxf
+cat $cx | $uglify > $cxm
+cat $cxf | $uglify > $cxfm
